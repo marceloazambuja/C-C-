2ba4b3a1f27ee08150e9bc848c62d713f97d46a5
marceloazambuja <marcelo.azambuja@gmail.com>
2018-07-03 10:08:31 -0300

    Initial Commit

---
+++ stream2file/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stream2file
+//
+//  Created by Marcelo Cunha de Azambuja on 7/3/18.
+//  Copyright © 2018 Marcelo Cunha de Azambuja. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
